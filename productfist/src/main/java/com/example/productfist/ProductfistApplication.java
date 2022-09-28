737830ff7fcd56cc90c135c02e8cfc53cbfa2d1a
huyem <huyem1004@gmail.com>
2022-09-28 14:32:30 +0700

    origin

---
+++ productfist/src/main/java/com/example/productfist/ProductfistApplication.java
@@ -0,0 +1,13 @@
+package com.example.productfist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductfistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductfistApplication.class, args);
+	}
+
+}
